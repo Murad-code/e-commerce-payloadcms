ab9645bfd98dcf5eeb7d8e4ca4d9eeb95d7202d9
Murad Kamali <murad.coding@gmail.com>
2024-02-25 15:43:26 +0000

    reverts changes for npm and next config

--- next.config.js
+++ next.config.js
@@ -8,17 +8,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-      },
-      {
-        protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_SERVER_URL,
-      },
-    ],
+    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
+      .filter(Boolean)
+      .map(url => url.replace(/https?:\/\//, '')),
   },
   redirects,
   async headers() {
@@ -46,14 +38,6 @@ const nextConfig = {
     headers.push({
       source: '/(.*)',
       headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-        {
-          key: 'Access-Control-Allow-Headers',
-          value:
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        },
         {
           key: 'Content-Security-Policy',
           value: ContentSecurityPolicy,
